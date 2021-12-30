262d3655cc532419e017aeefc7cf6732a337b0f2
hao.li <hao.li@funplus.com>
2021-12-30 10:31:36 +0800

    feat: add prettier to project

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk';
-import { Command } from 'commander';
-import inquirer from 'inquirer';
-import fs from 'fs';
-import ora from 'ora';
-import symbols from 'log-symbols';
-import download from 'download-git-repo';
-import https from 'https';
-import semver from 'semver';
-import shell from 'shelljs';
-import { readFile } from 'fs/promises';
+import chalk from 'chalk'
+import { Command } from 'commander'
+import inquirer from 'inquirer'
+import fs from 'fs'
+import ora from 'ora'
+import symbols from 'log-symbols'
+import download from 'download-git-repo'
+import https from 'https'
+import semver from 'semver'
+import shell from 'shelljs'
+import { readFile } from 'fs/promises'
 
-const packageJson = JSON.parse(
-  await readFile(new URL('./package.json', import.meta.url)),
-);
+const packageJson = JSON.parse(await readFile(new URL('./package.json', import.meta.url)))
 
-let projectName;
+let projectName
 
 function init() {
   const program = new Command(packageJson.name)
@@ -24,43 +22,43 @@ function init() {
     .description(packageJson.description)
     .arguments('<directory>')
     .usage(`${chalk.green('<directory>')}`)
-    .action((name) => {
-      projectName = name;
+    .action(name => {
+      projectName = name
     })
     .allowUnknownOption()
     .on('--help', () => {
-      console.log();
-      console.log(`Only ${chalk.green('<directory>')} is required.`);
-      console.log();
+      console.log()
+      console.log(`Only ${chalk.green('<directory>')} is required.`)
+      console.log()
     })
-    .parse(process.argv);
+    .parse(process.argv)
 
   checkForLatestVersion()
     .catch(() => {
       try {
-        return shell.exec('npm view create-fun-cli version').toString().trim();
+        return shell.exec('npm view create-fun-cli version').toString().trim()
       } catch (e) {
-        return null;
+        return null
       }
     })
-    .then((latest) => {
+    .then(latest => {
       if (latest && semver.lt(packageJson.version, latest)) {
-        console.log();
+        console.log()
         console.error(
           chalk.yellow(
             `You are running "${program.name()}@${
               packageJson.version
             }", which is behind the latest release (${latest}), please reinstall the latest version package.`,
           ),
-        );
-        console.log();
+        )
+        console.log()
         console.log(
           'Please remove any global installs with one of the following commands:\n' +
             `- npm uninstall -g ${program.name()}\n` +
             `- yarn global remove ${program.name()}`,
-        );
-        console.log();
-        process.exit(1);
+        )
+        console.log()
+        process.exit(1)
       } else {
         if (!fs.existsSync(projectName)) {
           inquirer
@@ -78,77 +76,71 @@ function init() {
                 default: true,
               },
             ])
-            .then((answers) => {
-              const repoName = getRepoName(answers);
-              const spinner = ora('Downloading...');
+            .then(answers => {
+              const repoName = getRepoName(answers)
+              const spinner = ora('Downloading...')
 
-              spinner.start();
+              spinner.start()
 
               download(repoName, projectName, function (err) {
                 if (err) {
-                  spinner.error();
-                  process.exit(1);
+                  spinner.error()
+                  process.exit(1)
                 } else {
-                  spinner.succeed();
-                  console.log(
-                    symbols.success,
-                    chalk.green('The object has been created successfully!'),
-                  );
+                  spinner.succeed()
+                  console.log(symbols.success, chalk.green('The object has been created successfully!'))
                 }
-              });
-            });
+              })
+            })
         } else {
-          console.log(symbols.error, chalk.red('Directory exists'));
+          console.log(symbols.error, chalk.red('Directory exists'))
         }
       }
-    });
+    })
 }
 
 function getRepoName(answers) {
-  const { type, isTsSupported } = answers;
-  let repo = 'github:create-fun-cli/';
+  const { type, isTsSupported } = answers
+  let repo = 'github:create-fun-cli/'
 
   if (type === 'react' && !isTsSupported) {
-    return repo + 'react-template';
+    return repo + 'react-template'
   } else if (type === 'react' && isTsSupported) {
-    return repo + 'react-ts-template';
+    return repo + 'react-ts-template'
   } else if (type === 'vue' && !isTsSupported) {
-    return repo + 'vue-template';
+    return repo + 'vue-template'
   } else if (type === 'vue' && isTsSupported) {
-    return repo + 'vue-ts-template';
+    return repo + 'vue-ts-template'
   }
 }
 
 function checkForLatestVersion() {
   return new Promise((resolve, reject) => {
     https
-      .get(
-        'https://registry.npmjs.org/-/package/create-fun-cli/dist-tags',
-        (res) => {
-          if (res.statusCode === 200) {
-            let data = '';
-            res.on('data', (chunk) => (data += chunk));
-            res.on('end', () => {
-              try {
-                const latest = JSON.parse(data || '{}')?.latest;
-                if (latest) {
-                  resolve(latest);
-                } else {
-                  reject();
-                }
-              } catch (err) {
-                throw err;
+      .get('https://registry.npmjs.org/-/package/create-fun-cli/dist-tags', res => {
+        if (res.statusCode === 200) {
+          let data = ''
+          res.on('data', chunk => (data += chunk))
+          res.on('end', () => {
+            try {
+              const latest = JSON.parse(data || '{}')?.latest
+              if (latest) {
+                resolve(latest)
+              } else {
+                reject()
               }
-            });
-          } else {
-            reject();
-          }
-        },
-      )
+            } catch (err) {
+              throw err
+            }
+          })
+        } else {
+          reject()
+        }
+      })
       .on('error', () => {
-        reject();
-      });
-  });
+        reject()
+      })
+  })
 }
 
-init();
+init()
